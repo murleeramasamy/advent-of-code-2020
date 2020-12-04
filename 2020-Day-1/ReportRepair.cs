bdf6cafd540bafc8c96609a712fbfff806463187
Murali Ramasamy <musomanu@microsoft.com>
2020-12-03 18:54:47 -0800

    Day 1 changes

---
+++ 2020-Day-1/ReportRepair.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode._2020_Day_1
+{
+    public class ReportRepair
+    {
+        private readonly HashSet<int> reportedNumbers;
+
+        public ReportRepair(string filePath)
+        {
+            this.reportedNumbers = new HashSet<int>();
+            this.readInput(filePath);
+        }
+
+        private void readInput(string filePath)
+        {
+            // TODO: Handle exception when file read fails
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                // TODO: Handle exception when the string can't be converted into an integer.
+                int number = Int32.Parse(line);
+
+                // TODO: Account for duplicate number
+                this.reportedNumbers.Add(number);
+            }
+        }
+
+        public int Find2EntriesProduct(int sum)
+        {
+           try
+            {
+                var (firstNumber, secondNumber) = this.FindEntriesThatAddupToSum(sum);
+
+                return firstNumber * secondNumber;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        private (int firstNumber, int secondNumber) FindEntriesThatAddupToSum(int sum)
+        {
+            foreach (int number in this.reportedNumbers)
+            {
+                int difference = sum - number;
+
+                if (this.reportedNumbers.Contains(difference))
+                {
+                    return (number, difference);
+                }
+            }
+
+            throw new Exception($"Cannot find 2 entries that add upto {sum}");
+        }
+
+        public int Find3EntriesProduct(int sum)
+        {
+            foreach (int firstNumber in this.reportedNumbers)
+            {
+                int difference = sum - firstNumber;
+
+                try
+                {
+                    var (secondNumber, thirdNumber) = this.FindEntriesThatAddupToSum(difference);
+
+                    return firstNumber * secondNumber * thirdNumber;
+                }
+                catch
+                {
+                    // do nothing, just try next number
+                }
+            }
+
+            throw new Exception($"Cannot find 3 entries that add upto {sum}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode._2020_Day_1;
+using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 2020;
+            string filePath = @"C:\Users\musomanu\source\advent-of-code-2020\2020-Day-1\ReportRepairInput.txt";
+            ReportRepair reportRepair = new ReportRepair(filePath);
+
+            var twoEntriesProduct = reportRepair.Find2EntriesProduct(sum);
+            var threeEntriesProduct = reportRepair.Find3EntriesProduct(sum);
+
+            Console.WriteLine(twoEntriesProduct);
+            Console.WriteLine(threeEntriesProduct);
+        }
+    }
+}
